33025ab152fbd3855416a7d673cad61590a8b968
ManuelPepe129 <manuelpepe1995@gmail.com>
2025-10-19 21:25:32 +0200

    Camera and player rotation at gravity switch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -31,7 +32,43 @@ public class PlayerController : MonoBehaviour
     {
         float angle = 90f * rotationValue;
         Physics.gravity = Quaternion.Euler(0f, 0f, angle) * Physics.gravity;
-        _camera.transform.Rotate(0, 0, angle);
+        StartCoroutine(PlayerCameraRotation(angle));
+    }
+
+    private IEnumerator PlayerCameraRotation(float angle)
+    {
+        _rigidbody.useGravity = false;
+        _rigidbody.linearVelocity = Vector3.zero;
+        _playerControls.Disable();
+        float duration = 2.0f;
+        float alpha = 0.0f;
+        Quaternion startPlayerRotation = _rigidbody.rotation;
+        Quaternion startCameraRotation = _camera.transform.rotation;
+        while (alpha < 1.0f)
+        {
+            float currentAngle = Mathf.LerpAngle(0, angle, alpha);
+            _rigidbody.rotation = Quaternion.Euler(0, 0, currentAngle) * startPlayerRotation;
+            _camera.transform.rotation = Quaternion.Euler(0, 0, currentAngle) * startCameraRotation;
+            alpha += Time.deltaTime / duration;
+            yield return null;
+        }
+
+        _rigidbody.rotation = Quaternion.Euler(0, 0, angle) * startPlayerRotation;
+        _camera.transform.rotation = Quaternion.Euler(0, 0, angle) * startCameraRotation;
+
+
+        _rigidbody.useGravity = true;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var collisionDirection = other.impulse.normalized;
+        float dot = -Vector3.Dot(Physics.gravity.normalized, collisionDirection);
+        if (dot > 0.5f)
+        {
+            _playerControls.Enable();
+            _isJumping = false;
+        }
     }
 
     private void FixedUpdate()
@@ -74,7 +111,7 @@ public class PlayerController : MonoBehaviour
         if (!_isJumping)
         {
             _rigidbody.AddForce(transform.up * jumpSpeed, ForceMode.Impulse);
-            // throw new NotImplementedException();
+            _isJumping = true;
         }
     }
 }

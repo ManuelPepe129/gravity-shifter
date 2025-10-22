b63e0be5fa4295900d63b7ca582e8f02ade8472f
Chiara De Giorgi <chiaradg@lecceinrete.it>
2025-10-22 17:29:50 +0200

    Ghost puff

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    [SerializeField] GameObject ghost;
+    [SerializeField] GameObject ghostRoot;
     [SerializeField] Transform startingPoint;
     [SerializeField] Transform endPoint;
     [SerializeField] float speedMultiplier = 1.0f;
+
+    GameObject ghost;
+    GameObject particles;
+
+    private void Awake()
+    {
+        ghost = ghostRoot.GetComponentInChildren<MeshRenderer>(true).gameObject;
+        particles = ghostRoot.GetComponentInChildren<ParticleSystem>(true).gameObject;
+    }
 
     /// <summary>
     /// When the player collides with the trigger
@@ -17,9 +26,25 @@ public class EnemySpawn : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            ghost.SetActive(true);
-            StartCoroutine(MoveEnemy(startingPoint, endPoint));
+            StartCoroutine(TriggerSequence());
         }
+    }
+
+    /// <summary>
+    /// 1. Activate particles
+    /// 2. Activate ghost
+    /// 3. Start moving
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator TriggerSequence()
+    {
+        particles.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+
+        ghost.SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        yield return StartCoroutine(MoveEnemy(startingPoint, endPoint));
     }
 
     private IEnumerator MoveEnemy(Transform from, Transform to)
@@ -32,15 +57,15 @@ public class EnemySpawn : MonoBehaviour
         while (elapsedTime < movementDuration)
         {
             Vector3 currentPosition = Vector3.Lerp(from.position, to.position, alpha);
-            ghost.transform.position = currentPosition;
+            ghostRoot.transform.position = currentPosition;
             alpha += Time.deltaTime / movementDuration;
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
         // Final destination + switch rotation
-        ghost.transform.position = to.position;
-        ghost.transform.Rotate(new Vector3(0, 1, 0), 180);
+        ghostRoot.transform.position = to.position;
+        ghostRoot.transform.Rotate(new Vector3(0, 1, 0), 180);
 
         // Invert from and to
         from = (from == startingPoint) ? endPoint : startingPoint;

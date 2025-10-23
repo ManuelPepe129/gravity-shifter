4878950e9a45186c489c4987089945d944e7fbb9
Chiara De Giorgi <chiaradg@lecceinrete.it>
2025-10-23 15:11:32 +0200

    Candy explosion and fence added

--- Assets/Scripts/PumpkinParticle.cs
+++ Assets/Scripts/PumpkinParticle.cs
@@ -3,6 +3,20 @@ using UnityEngine;
 public class PumpkinParticle : MonoBehaviour
 {
     [SerializeField] ParticleSystem PumpkinParticles;
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (animator != null)
+        {
+            
+        }
+    }
 
     public void StartLeafExplosion()
     {

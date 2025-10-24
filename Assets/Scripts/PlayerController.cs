525ceb4a29c4383fcf7aa9d1c6edd3c4491e825b
ManuelPepe129 <manuelpepe1995@gmail.com>
2025-10-24 13:14:29 +0200

    Add camera reversed animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -35,8 +35,7 @@ public class PlayerController : MonoBehaviour
     /// <param name="rotationValue"></param>
     private void RotateGravity(float rotationValue)
     {
-        float angle = rotationValue > 0 ? 90f : -90f;
-        //float angle = 90f * rotationValue;
+        float angle = 90f * rotationValue;
         Physics.gravity = Quaternion.Euler(0f, 0f, angle) * Physics.gravity;
         StartCoroutine(PlayerCameraRotation(angle));
     }
@@ -53,7 +52,14 @@ public class PlayerController : MonoBehaviour
         _rigidbody.linearVelocity = Vector3.zero;
         _playerControls.Disable();
 
-        _camera.GetComponent<Animator>().Play("Camera Gravity Shift");
+        if (angle > 0)
+        {
+            _camera.GetComponent<Animator>().Play("Camera Gravity Shift");
+        }
+        else
+        {
+            _camera.GetComponent<Animator>().Play($"Camera Gravity Shift_Reversed");
+        }
 
         //yield return new WaitForSeconds(.2f);
         float duration = 2.0f;
